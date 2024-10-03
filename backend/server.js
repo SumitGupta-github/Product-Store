84e7fdcc71387d95d16f82db7d3b80e089314901
Sumit Gupta <gsumit9144@gmail.com>
2024-10-03 05:31:27 +0530

    Project-Completed

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-import connectDB from './config/db.js';
-import productRoutes from './routes/product.route.js'
+import  connectDB  from "./config/db.js";
 
-const app = express()
-dotenv.config()
-const Port = process.env.PORT || 5000
+import productRoutes from "./routes/product.route.js";
 
-app.use(express.json()) // allows us to accecpt JSON data in the json.body
+dotenv.config();
 
-//Products Routes 
-app.use('/api/Product', productRoutes)
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(Port, ()=> {
-    connectDB()
-    console.log(`Server started at http://localhost:${Port}`);
-})
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/Product", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
